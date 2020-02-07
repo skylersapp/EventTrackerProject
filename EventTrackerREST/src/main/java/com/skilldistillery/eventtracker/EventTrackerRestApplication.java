d2dc41ae0e7f68962f07913deac499123683f8eb
skylersapp <skylersapp@icloud.com>
2020-02-07 16:36:57 -0700

    Stub JPA and REST projects

---
+++ EventTrackerREST/src/main/java/com/skilldistillery/eventtracker/EventTrackerRestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerRestApplication.class, args);
+	}
+
+}
